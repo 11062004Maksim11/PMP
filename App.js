03be2761259075d501087940685bfd1ac930554e
Yarik22 <popov.yarik.popov@gmail.com>
2023-09-10 19:42:55 +0300

    lr3

--- App.js
+++ App.js
@@ -1,51 +1,21 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  Alert,
-  Button,
-  TouchableHighlight,
-} from "react-native";
+import ButtonCounter from "./components/ButtonCounter";
+import React, { useState } from "react";
 
 export default function App() {
-  const handleButtonPress = () => {
-    console.log("Button pressed");
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
   };
-  const handleTextPress = () =>
-    Alert.alert("Press OK", "Please", [
-      {
-        text: "OK",
-        onPress: () => console.log("Congrats!!!"),
-      },
-    ]);
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
 
   return (
-    <View style={styles.container}>
-      <TouchableHighlight
-        activeOpacity={0.6}
-        onPress={handleTextPress}
-        underlayColor="#DDDDDD"
-      >
-        <Text numberOfLines={1} style={styles.text}>
-          Click me
-        </Text>
-      </TouchableHighlight>
-      <Button onPress={handleButtonPress} title="Button" color={"orange"} />
-    </View>
+    <ButtonCounter
+      count={count}
+      incrementCount={incrementCount}
+      decrementCount={decrementCount}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "green",
-    fontSize: 24,
-  },
-});
