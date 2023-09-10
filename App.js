56363acfe0e8320853388a46f5dd506ef3bf1627
Yarik22 <popov.yarik.popov@gmail.com>
2023-09-10 18:01:00 +0300

    Clickable

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Alert,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
+  const handleButtonPress = () => {
+    console.log("Button pressed");
+  };
+  const handleTextPress = () =>
+    Alert.alert("Press OK", "Please", [
+      {
+        text: "OK",
+        onPress: () => console.log("Congrats!!!"),
+      },
+    ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableHighlight
+        activeOpacity={0.6}
+        onPress={handleTextPress}
+        underlayColor="#DDDDDD"
+      >
+        <Text numberOfLines={1} style={styles.text}>
+          Click me
+        </Text>
+      </TouchableHighlight>
+      <Button onPress={handleButtonPress} title="Button" color={"orange"} />
     </View>
   );
 }
@@ -13,8 +40,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "green",
+    fontSize: 24,
   },
 });
